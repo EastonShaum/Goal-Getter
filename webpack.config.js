48f4f8b068da6d3565e31bee20f118b24da06f24
Jake P <76881086+jbped@users.noreply.github.com>
2021-07-29 23:38:05 -0600

    updated webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,24 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 const webpack = require("webpack")
 
 module.exports = {
-    entry: './src/app.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'public/assets/js')
-    },
     mode: "production",
+    entry: {
+        customBootstrap: "./src/app.js",
+    },
+    output: {
+        filename: '[name]',
+        path: path.resolve(__dirname, 'public/assets/css')
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "[name].css"
+        })
+    ],
+    devServer: {
+        contentBase: path.join(__dirname, "public"),
+    },
     module: {
         rules: [
             {
@@ -15,24 +26,13 @@ module.exports = {
                 use: [{
                     // inject CSS to page
                     loader: 'style-loader'
-                }, {
+                }, 
+                {
+                    loader: MiniCssExtractPlugin.loader
+                },                
+                {
                     // translates CSS into CommonJS modules
                     loader: 'css-loader'
-                }, {
-                    // Run postcss actions
-                    loader: 'postcss-loader',
-                    options: {
-                        // `postcssOptions` is needed for postcss 8.x;
-                        // if you use postcss 7.x skip the key
-                        postcssOptions: {
-                            // postcss plugins, can be exported to postcss.config.js
-                            plugins: function () {
-                                return [
-                                    require('autoprefixer')
-                                ];
-                            }
-                        }
-                    }
                 }, {
                     // compiles Sass to CSS
                     loader: 'sass-loader'
@@ -40,11 +40,5 @@ module.exports = {
                 ]
             }
         ]
-    },
-    // plugins: [
-    //     new webpack.ProvidePlugin({
-    //         $: "jquery",
-    //         jQuery: "jquery"
-    //     })
-    // ]
+    }
 };
