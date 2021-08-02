19d9badb36a7a371c9d75d9557f7f5627ecd6cf4
Jake P <76881086+jbped@users.noreply.github.com>
2021-08-01 23:57:27 -0600

    new goal modal, flatpickr, need to get vals from elements for submit

--- public/assets/js/newGoal.js
+++ public/assets/js/newGoal.js
@@ -1,5 +1,77 @@
-// const toggleModal = () => {
-//     $("#new-goal-modal").toggle()
-// }
+let today = new Date()
+let tomorrow =  new Date()
+tomorrow.setDate(today.getDate() + 1)
 
-// $("#new-goal-btn").on("click", toggleModal)
+let dueDate
+
+const flatpickrConfig = {
+    altInput: true,
+    altFormat: "F j, Y",
+    dateFormat: "Z",
+    minDate: tomorrow,
+    onClose: function(selectedDates, dateStr, instance) {
+        dueDate = dateStr
+        console.log(dueDate)
+    }
+}
+
+const calendar = new flatpickr("#due-date-input", flatpickrConfig)
+
+$(".modal").on("hidden.bs.modal", function(){
+    $("#goal-title-input").val("");
+    $("#due-date-input").val("");
+    $("#goal-description-input").val("");
+    $("#form-check-input").val("");
+});
+
+async function addGoalHandler(event) {
+    event.preventDefault();
+
+    const title = $('#goal-title-input').val();
+    const description = $('#due-date-input').val();
+
+    // can use the jquery datepicker function to turn a modal into a calendar
+    const due_date = dueDate
+
+    // Use a radio button for this so that a boolean value can be used
+    const is_public = $('#public-goal-checkbox').val();
+
+    // const user_id = $("new-goal-form").attr("user-id").val();
+
+    console.log({
+        title,
+        description,
+        due_date,
+        is_public,
+        // user_id
+    })
+
+    if (title && description && is_public) {
+        console.log({
+            title,
+            description,
+            due_date,
+            is_public,
+            // user_id
+        })
+        // const response = await fetch('/api/goals', {
+        //     method: 'POST',
+        //     body: JSON.stringify({
+        //         title,
+        //         description,
+        //         due_date,
+        //         is_public,
+        //         user_id
+        //     }),
+        //     headers: { 'Content-Type': 'application/json' }
+        // });
+
+        // if (response.ok) {
+        //     console.log('success');
+        // } else {
+        //     alert(response.statusText);
+        // }
+    }
+}
+
+$("#new-goal-form").on("submit", addGoalHandler);
